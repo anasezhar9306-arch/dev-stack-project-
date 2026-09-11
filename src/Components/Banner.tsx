9183ade71ea984551032bca9f184f5d1a9c5994b
anasezhar9306-arch <anasezhar9306@gmail.com>
2026-09-11 20:09:48 +0600

    Complete responsive Banner

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,65 +1,40 @@
 import BannerIMG from '../assets/banner-stack.png'
 const Banner = () => {
     return (
-        <div className='flex justify-between max-w-6xl mx-auto'>
-            
-            <div className='flex flex-col justify-center gap-9'>
+
+        <div className='bg-white flex flex-col md:flex-row justify-between items-center px-4 max-w-6xl mx-auto gap-7 md:gap-11 mt-10 md:mt-4'>
+
+
+            <div className='flex flex-col justify-center items-center md:items-start w-full md:w-1/2 gap-6 md:gap-11'>
+
+
                 {/* Banner Text */}
+                <div className='flex flex-col gap-y-5 text-center md:text-left'>
+                    <h2 className='text-3xl md:text-6xl font-bold md:mb-3'>Build Your Ideal<br />
+                        <span className='bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent leading-tight'>Development Stack</span>
+                    </h2>
 
-                <h2 className='text-6xl font-bold'>Build Your Ideal<br/>
-                <span className='bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent'>Development Stack</span>
-                </h2>
+                    <p>Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your
+                        next project.</p>
+                </div>
 
-                <p>Explore frontend, backend, database, and tooling options,<br/>
-                   compare them side by side, and put together the stack that fits your<br/> 
-                   next project.</p>
-                
                 {/* Banner Button */}
-                
-                <div className='flex gap-2.5'>
+                <div className='flex gap-2.5 justify-center md:justify-start'>
 
                     <button className='rounded-md bg-linear-to-r from-[#F97316] to-[#EC4899] py-1.5 px-2 text-white'>Explore Technologies</button>
 
-                    <button className='border rounded-md py-1.5 px-10'>Learn More</button>
+                    <button className='border rounded-md py-1.5 px-6 md:px-10'>Learn More</button>
 
                 </div>
 
             </div>
 
             {/* Banner IMG */}
-            <img src={BannerIMG} alt="Banner img" />
+            <div className='w-full max-w-md md:max-w-lg flex justify-center md:justify-end'>
+                <img src={BannerIMG} alt="Banner img" className='max-w-md md:max-w-xl w-full' />
+            </div>
         </div>
     );
 };
 
 export default Banner;
-
-
-        // <div className='flex justify-between max-w-6xl mx-auto'>
-            
-        //     <div className='flex flex-col justify-center gap-9'>
-        //         {/* Banner Text */}
-
-        //         <h2 className='text-6xl font-bold'>Build Your Ideal<br/>
-        //         <span className='bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent'>Development Stack</span>
-        //         </h2>
-
-        //         <p>Explore frontend, backend, database, and tooling options,<br/>
-        //            compare them side by side, and put together the stack that fits your<br/> 
-        //            next project.</p>
-                
-        //         {/* Banner Button */}
-                
-        //         <div className='flex gap-2.5'>
-
-        //             <button className='rounded-md bg-linear-to-r from-[#F97316] to-[#EC4899] py-1.5 px-2 text-white'>Explore Technologies</button>
-
-        //             <button className='border rounded-md py-1.5 px-10'>Learn More</button>
-
-        //         </div>
-
-        //     </div>
-
-        //     {/* Banner IMG */}
-        //     <img src={BannerIMG} alt="Banner img" />
-        // </div>
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -3,26 +3,26 @@ import Logo from '../assets/logo-text.png'
 
 const Nav = () => {
     return (
-        <div className='sticky top-0 z-50 bg-fuchsia-100 py-1.5'>
+        <div className='sticky top-0 z-50 bg-fuchsia-100 py-1.5 px-1'>
             <div className='flex justify-between items-center max-w-6xl mx-auto mt-1 md:mt-1.5 mb-1'>
 
-            <RxHamburgerMenu className='block md:hidden w-7 h-11' />
-            
-            <img src={Logo} alt="Logo" />
+                <RxHamburgerMenu className='block md:hidden w-7 h-11' />
 
-            <ul className='hidden md:flex gap-4 font-mono'>
-                <li className='text-[#D91B7E]'>Home</li>
-                <li>Technologies</li>
-                <li>Projects</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-            <div className='flex gap-1.5 md:gap-2 font-semibold md:font-normal'>
-                <button className='text-sm md:text-base font-semibold'>Sign in</button>
-                <button className='rounded-3xl bg-[#D91B7E] text-white text-sm md:text-base py-1 md:py-1.5 px-2 md:px-3'>Sign Up</button>
+                <img src={Logo} alt="Logo" />
+
+                <ul className='hidden md:flex gap-4 font-mono'>
+                    <li className='text-[#D91B7E]'>Home</li>
+                    <li>Technologies</li>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                <div className='flex gap-1.5 md:gap-2 font-semibold md:font-normal'>
+                    <button className='text-sm md:text-base font-semibold'>Sign in</button>
+                    <button className='rounded-3xl bg-[#D91B7E] text-white text-sm md:text-base py-1 md:py-1.5 px-2 md:px-3'>Sign Up</button>
+                </div>
+
             </div>
-            
-        </div>
         </div>
     );
 };
