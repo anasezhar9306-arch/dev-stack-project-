2d7b52eb0f896b0d1ad6a0e3a705000a37e9c65d
anasezhar9306-arch <anasezhar9306@gmail.com>
2026-09-11 08:00:31 +0600

    Nav complete

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Nav from "./Components/Nav"
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav/>
+      {/* <div className="divider"/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,28 @@
+import { RxHamburgerMenu } from 'react-icons/rx';
+import Logo from '../assets/logo-text.png'
+
+const Nav = () => {
+    return (
+        <div className='sticky top-0 z-50 flex justify-between items-center max-w-6xl mx-auto mt-1 md:mt-2.5'>
+
+            <RxHamburgerMenu className='block md:hidden w-7 h-11' />
+            
+            <img src={Logo} alt="Logo" />
+
+            <ul className='hidden md:flex gap-3'>
+                <li className='text-[#D91B7E]'>Home</li>
+                <li>Technologies</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <div className='flex gap-1.5 md:gap-2 font-semibold md:font-normal'>
+                <button className='text-sm md:text-base'>Sign in</button>
+                <button className='rounded-3xl bg-[#D91B7E] text-white text-sm md:text-base py-1 md:py-1.5 px-2 md:px-3'>Sign Up</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Nav;
